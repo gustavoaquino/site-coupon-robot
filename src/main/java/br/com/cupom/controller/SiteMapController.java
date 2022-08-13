033fd3709d964e914bce5be1f90920ea0ca8627a
gustavoaquino <guuaquino@hotmail.com>
2022-08-13 13:59:05 -0300

    adição de dependencia para criar o sitemap

--- src/main/java/br/com/cupom/controller/SiteMapController.java
+++ src/main/java/br/com/cupom/controller/SiteMapController.java
@@ -4,13 +4,15 @@ import br.com.cupom.controller.to.sitemap.XmlUrl;
 import br.com.cupom.controller.to.sitemap.XmlUrlSet;
 import br.com.cupom.domain.Company;
 import br.com.cupom.service.CompanyService;
+import cz.jiripinkas.jsitemapgenerator.WebPage;
+import cz.jiripinkas.jsitemapgenerator.generator.SitemapGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -20,30 +22,54 @@ public class SiteMapController {
     @Autowired
     private CompanyService companyService;
 
-    @GetMapping({"/sitemap.xml", "/Sitemap.xml"})
+    @GetMapping(value = {"/sitemap.xml", "/Sitemap.xml"}, produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
-    public XmlUrlSet main() {
-        final List<String> urlList = new ArrayList<>();
-        urlList.add("/");
-        urlList.add( "/lojas");
-        urlList.add( "/ajuda");
+    public String main() {
+        final SitemapGenerator sitemapGenerator = SitemapGenerator.of(DOMAIN);
 
         final List<Company> companyList = this.companyService.findAllComapaniesOrderByName();
 
-        final List<String> companyUrlSiteMapList = new ArrayList<>();
+        final List<String> urlList = new ArrayList<>();
+        urlList.add("/");
+        urlList.add("/lojas");
+        urlList.add("/ajuda");
 
+        final List<String> companyUrlSiteMapList = new ArrayList<>();
         for(Company company : companyList){
             companyUrlSiteMapList.add("/lojas/" + company.getUriSocialSoul());
         }
 
         urlList.addAll(companyUrlSiteMapList);
 
-        XmlUrlSet xmlUrlSet = new XmlUrlSet();
-        for (String eachLink : urlList) {
-            create(xmlUrlSet, eachLink, XmlUrl.Priority.HIGH);
+
+        for (String url : urlList) {
+            sitemapGenerator.addPage(url);
+            sitemapGenerator.addPage(WebPage.builder().name(url).maxPriorityRoot().changeFreqDaily().lastModNow().build());
         }
 
-        return xmlUrlSet;
+
+
+//        final List<String> urlList = new ArrayList<>();
+//        urlList.add("/");
+//        urlList.add( "/lojas");
+//        urlList.add( "/ajuda");
+//
+//        final List<Company> companyList = this.companyService.findAllComapaniesOrderByName();
+//
+//        final List<String> companyUrlSiteMapList = new ArrayList<>();
+//
+//        for(Company company : companyList){
+//            companyUrlSiteMapList.add("/lojas/" + company.getUriSocialSoul());
+//        }
+//
+//        urlList.addAll(companyUrlSiteMapList);
+//
+//        XmlUrlSet xmlUrlSet = new XmlUrlSet();
+//        for (String eachLink : urlList) {
+//            create(xmlUrlSet, eachLink, XmlUrl.Priority.HIGH);
+//        }
+
+        return sitemapGenerator.toString();
     }
 
     private void create(XmlUrlSet xmlUrlSet, String link, XmlUrl.Priority priority) {
