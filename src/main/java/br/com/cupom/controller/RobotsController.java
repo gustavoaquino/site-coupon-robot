d25357c12ebbbbd793ef2a2ce1847ee143943295
gustavoaquino <guuaquino@hotmail.com>
2022-08-13 09:54:42 -0300

    Atualizando robot controller.

--- src/main/java/br/com/cupom/controller/RobotsController.java
+++ src/main/java/br/com/cupom/controller/RobotsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RobotsController {
 
-    @GetMapping(value = {"/robots.txt", "/robot.txt"})
+    @GetMapping(value = {"/robots.txt", "/robot.txt", "/Robots.txt", "/Robot.txt"})
     @ResponseBody
     public String getRobotsTxt() {
         return "User-agent: *";
