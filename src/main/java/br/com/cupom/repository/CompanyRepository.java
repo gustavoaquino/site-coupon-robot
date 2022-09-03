763016810dfb6af56f9becd3263a6712eeecc8fc
gustavoaquino <guuaquino@hotmail.com>
2022-09-02 21:40:04 -0300

    Retirando schema.

--- src/main/java/br/com/cupom/repository/CompanyRepository.java
+++ src/main/java/br/com/cupom/repository/CompanyRepository.java
@@ -11,13 +11,13 @@ import java.util.Optional;
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-    @Query(value = "SELECT * FROM CUPOM.COMPANY ORDER BY ID ASC LIMIT 6", nativeQuery = true)
+    @Query(value = "SELECT * FROM COMPANY ORDER BY ID ASC LIMIT 6", nativeQuery = true)
     List<Company> findAllLimit6ByOrderByIdAsc();
 
-    @Query(value = "SELECT * FROM CUPOM.COMPANY ORDER BY NAME_COMPANY ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM COMPANY ORDER BY NAME_COMPANY ASC", nativeQuery = true)
     List<Company> findAllCompanyByOrderByNameCompanyAsc();
 
-    @Query(value = "SELECT * FROM CUPOM.COMPANY WHERE URI_SOCIAL_SOUL = ? ", nativeQuery = true)
+    @Query(value = "SELECT * FROM COMPANY WHERE URI_SOCIAL_SOUL = ? ", nativeQuery = true)
     Optional<Company> findCompanyByUriSocialSoul(String uriSocialSoul);
 
 }
